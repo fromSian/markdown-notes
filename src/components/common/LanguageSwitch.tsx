4fd51525c134986a0e0b4fb0f6078aa073086580
fromsian <fromSian@163.com>
2024-06-23 17:05:39 +0800

    feat: i18n

---
+++ src/components/common/LanguageSwitch.tsx
@@ -0,0 +1,44 @@
+import { useTranslation } from "react-i18next";
+
+import Cookies from "js-cookie";
+import { ChangeEvent, useEffect, useState, useTransition } from "react";
+
+import { languages } from "@/i18";
+const ThemeSwitch = () => {
+  const [mounted, setMounted] = useState(false);
+
+  const { t, i18n } = useTranslation();
+  const [currentLng, setCurrentLng] = useState(
+    Cookies.get("i18next") || languages[0]
+  );
+  const [isPending, startTransition] = useTransition();
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  const onLanguageChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    startTransition(() => {
+      i18n.changeLanguage(e.target.value);
+
+      setCurrentLng(e.target.value);
+    });
+  };
+
+  return (
+    <select value={currentLng} onChange={onLanguageChange}>
+      {languages.map((language) => (
+        <option key={language} value={language}>
+          {t(language)}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default ThemeSwitch;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
+import LanguageSwitch from "@/components/common/LanguageSwitch";
+import ThemeSwitch from "@/components/common/ThemeSwitch";
 import { store } from "@/states/store";
 import { ThemeProvider } from "next-themes";
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ThemeSwitch from "./components/common/ThemeSwitch";
 import "./global.css";
 const lazyLoad = (path: string) => {
   if (path.startsWith("@/")) {
@@ -30,6 +31,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
       <ThemeProvider attribute="class">
         <ThemeSwitch />
+        <LanguageSwitch />
         <RouterProvider router={router} />
       </ThemeProvider>
     </Provider>
