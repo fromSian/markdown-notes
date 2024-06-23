8a6f64dc006e8b9471dae85e5e9e38e27145d0aa
fromsian <fromSian@163.com>
2024-06-23 16:06:58 +0800

    feat: react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./global.css";
+
+const lazyLoad = (path: string) => {
+  if (path.startsWith("@/")) {
+    path = path.replace("@/", "");
+  }
+  const Module = lazy(async () => import(/* @vite-ignore */ `/src/${path}`));
+  return (
+    <Suspense fallback={"loading"}>
+      <Module />
+    </Suspense>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: lazyLoad("@/pages"),
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
