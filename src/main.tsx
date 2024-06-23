a8a38557c3564136421468d33a72612c508f8335
fromsian <fromSian@163.com>
2024-06-23 16:14:13 +0800

    feat: redux/toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
+import { store } from "@/states/store";
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./global.css";
 
@@ -24,6 +26,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
