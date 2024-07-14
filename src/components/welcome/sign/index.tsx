53b840f159808025967e5144c9e731f4933bfeeb
fromsian <fromSian@163.com>
2024-07-15 02:56:45 +0800

    feat: sign trial

--- src/components/welcome/sign/index.tsx
+++ src/components/welcome/sign/index.tsx
@@ -1,14 +1,35 @@
+import request from "@/request/request";
 import { useState } from "react";
+import { useNavigate } from "react-router";
+import { toast } from "sonner";
 import SignIn from "./signin";
 import SignUp from "./signup";
 
 const Sign = () => {
+  const navigate = useNavigate();
   const [open, setOpen] = useState<"signin" | "signup" | undefined>(undefined);
 
   const goSomeWhereElse = () => {
     setOpen(undefined);
   };
 
+  const goGoogleAuth = () => {
+    const url = "http://localhost:8000/account/google/access/";
+    window.open(url, "_self");
+  };
+  const handleGoogleAuth = () => {
+    goGoogleAuth();
+    setOpen(undefined);
+  };
+
+  const handleTrial = async () => {
+    setOpen(undefined);
+    const url = "/account/trial/";
+    const response = await request.post(url);
+    sessionStorage.setItem("token", response?.token);
+    toast.success("trial successfully");
+    navigate("/notes");
+  };
   return (
     <div className="flex flex-col w-full p-4">
       <button className="black_btn mb-4" onClick={() => setOpen("signin")}>
@@ -19,10 +40,10 @@ const Sign = () => {
         sign up
       </button>
       <SignUp open={open === "signup"} goSomeWhereElse={goSomeWhereElse} />
-      <button className="black_btn mb-4" onClick={() => setOpen(undefined)}>
+      <button className="black_btn mb-4" onClick={handleGoogleAuth}>
         sign in with google
       </button>
-      <button className="black_btn mb-4" onClick={() => setOpen(undefined)}>
+      <button className="black_btn mb-4" onClick={handleTrial}>
         trial
       </button>
     </div>
